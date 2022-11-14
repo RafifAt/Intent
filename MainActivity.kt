41b22df9c1e90548ca4cebf6d56cda40088e9cf5
Muhammad Rafif <101534510+RafifAt@users.noreply.github.com>
2022-11-14 08:14:14 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.coba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.SimpleCursorAdapter.ViewBinder
+import com.example.myintent.R
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button =
+            findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.btn_move_activity ->{
+
+            }
+        }
+    }
+}
